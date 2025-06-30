86111e90b4f3fb16a185cd9980e4eed39db986cf
LahiruWijesundara <lmwijesundara@gmail.com>
2025-06-30 12:13:35 +0000

    Deploying to gh-pages from @ LahiruWijesundara/lahiruwijesundara.github.io@383b8767a351bf316be23006dbbf6257d47a2646 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,26 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{id: "nav-work-experience",
           title: "Work-Experience",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/work-experience/";
+          },
+        },{id: "nav-people",
+          title: "people",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
           },
         },{id: "post-a-post-with-plotly-js",
         
